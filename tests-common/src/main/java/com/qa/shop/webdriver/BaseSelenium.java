a4972b5aa1054edf9dbd9df4b3773aa032f8578d
artem.lezgyan <artem.lezgyan@exactprosystems.com>
2022-03-27 00:09:59 +0400

    change parameters

--- tests-common/src/main/java/com/qa/shop/webdriver/BaseSelenium.java
+++ tests-common/src/main/java/com/qa/shop/webdriver/BaseSelenium.java
@@ -10,6 +10,8 @@ import java.time.Duration;
 
 public class BaseSelenium {
 
+    private static final Integer IMPLICIT_WAIT = 45;
+
     private static WebDriver driver;
 
     private static final String path = new File("screens").getAbsolutePath();
@@ -37,7 +39,7 @@ public class BaseSelenium {
 //        options.addArguments("user-agent=Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 640 XL LTE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10166");
 //        options.setExperimentalOption("useAutomationExtension", false);
         driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICIT_WAIT));
     }
 
     public static void close() {
